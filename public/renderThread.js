e2ee60df5bbb1244a1326bea905303dfe63d87dd
Wallace McGee <wm@wolispace.com>
2026-03-21 13:41:22 +1030

    feat: panning and colours

--- public/renderThread.js
+++ public/renderThread.js
@@ -18,31 +18,47 @@ self.onmessage = e => {
     
     // gameCanvasOffscreenContext.stroke();
 
-    const numElements = 10;
+    const numElements = 2000;
     const grid = {x: numElements, y: numElements};
+    
 
     const gridSab = new SharedArrayBuffer(Uint32Array.BYTES_PER_ELEMENT * grid.x * grid.y);
     const gridArray = new Uint32Array(gridSab); 
     console.log(gridArray);
     console.log(gridArray.length);
+    for (let gridIdx = 0; gridIdx < gridArray.length; gridIdx++) {
+        gridArray[gridIdx] = Math.random() * 255;
+    }
 
     function step(timestamp) {
         // console.log(Atomics.load(boundingCoordinatesArray, 0));
         // console.log(Atomics.load(boundingCoordinatesArray, 1));
         ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+        const leftLimit = Atomics.load(boundingCoordinatesArray, 0);
+        const topLimit = Atomics.load(boundingCoordinatesArray, 1);
+        const rightLimit = Atomics.load(boundingCoordinatesArray, 2);
+        const bottomLimit = Atomics.load(boundingCoordinatesArray, 3);
         
         for (let gridIdx = 0; gridIdx < gridArray.length; gridIdx++) {
-            const i = Math.floor(gridIdx/grid.x);
-            const j = gridIdx % grid.x;
-            ctx.beginPath();
-            ctx.arc(
-                i * hexRadius * 2 + hexRadius + j * hexRadius - Atomics.load(boundingCoordinatesArray, 0), 
-                j * hexRadius * 1.73205080757 + hexRadius - Atomics.load(boundingCoordinatesArray, 1), 
-                hexRadius, 
-                0, 
-                2 * Math.PI);
-            ctx.fillStyle = "wheat";
-            ctx.fill();
+            
+            const gridX = Math.floor(gridIdx/grid.x);
+            const gridY = gridIdx % grid.x;
+            const centerX = gridX * hexRadius * 2 + hexRadius + gridY * hexRadius ;
+            const centerY = gridY * hexRadius * 1.73205080757 + hexRadius;
+            if (centerX > leftLimit 
+                && centerX < rightLimit 
+                && centerY > topLimit
+                && centerY < bottomLimit) {
+                ctx.beginPath();
+                ctx.arc(
+                    centerX - leftLimit, 
+                    centerY - topLimit, 
+                    hexRadius, 
+                    0, 
+                    2 * Math.PI);
+                ctx.fillStyle = `rgb(${gridArray[gridIdx]},${gridArray[gridIdx]},${gridArray[gridIdx]})`;
+                ctx.fill();
+            }
         }
         requestAnimationFrame(step);
     }
