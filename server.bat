7df3a38ac19dd60b80e5e162b6b361dad63d7783
Wallace McGee <wm@wolispace.com>
2026-04-07 16:47:40 +0930

    chore: renaming dev html

--- server.bat
+++ server.bat
@@ -3,6 +3,6 @@ echo Starting server and browser
 :: open a web browser
 :: start node in watch mode so it restarts if there are code changes
 
-start "" https://localhost/dev.html
+start "" https://localhost/index.html
 start "" node --watch server.js
 
